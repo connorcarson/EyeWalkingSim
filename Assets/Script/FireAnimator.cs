4ad05ea1ffca44d31f4827f83a115701320f050f
Connor <cc3237@nyu.edu>
2018-10-16 15:49:10 -0400

    Trying this again: added door, window, eyes, eyelids, toilet and bookcase to scene. Uploaded some poly models, and made several textures/materials

---
+++ Assets/Script/FireAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class FireAnimator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		transform.DOMoveY(-38, 10).SetEase(Ease.InBounce);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
