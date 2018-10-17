3502507d2046664f80a600a2d92fd2b399732ee5
Yukii <yukiizzzx@gmail.com>
2018-10-18 02:49:35 +0800

    Add LightChasing level

---
+++ Assets/Script/LightChasing/LightChasingLevelController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightChasingLevelController : MonoBehaviour {
+
+    public bool isProceed = false;
+    public bool isFinished = false;
+    public GameObject player;
+    public GameObject chaseLight;
+
+    public Transform playerStartPoint;
+    public Transform playerEndPoint;
+    public Transform lightStartPoint;
+    public Transform lightEndPoint;
+
+    float startEndDistance;
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    void Update ()
+    {
+        if (isProceed && !isFinished)
+        {
+            float ratio = Mathf.Clamp01(Vector3.Distance(player.transform.position, playerStartPoint.position) / startEndDistance);
+            chaseLight.transform.position = lightEndPoint.position * ratio + lightStartPoint.position * (1 - ratio);
+
+            if (ratio > 0.99f)
+            {
+                isFinished = true;
+                GameOver();
+            }
+        }
+	}
+
+    void Initialize()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        chaseLight = GameObject.FindGameObjectWithTag("ChaseLight");
+        startEndDistance = Vector3.Distance(playerStartPoint.position, playerEndPoint.position);
+        isProceed = true;
+    }
+
+    void GameOver()
+    {
+        Debug.Log("GameOver");
+        //TODO
+    }
+}
