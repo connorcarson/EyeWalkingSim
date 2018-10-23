5c939096ec3c543f94d2e4d257d857b1e4133fff
Yukii <yukiizzzx@gmail.com>
2018-10-23 16:37:41 +0800

    Made some detailed effects in Hallway escape

---
+++ Assets/Script/HallwayRun/ExitDoorTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoorTrigger : MonoBehaviour
+{
+    public bool doorTriggered = false;
+    public GameObject player;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            doorTriggered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            doorTriggered = false;
+        }
+    }
+}
--- Assets/Script/HallwayRun/PlayerHallwayRun.cs
+++ Assets/Script/HallwayRun/PlayerHallwayRun.cs
@@ -8,9 +8,11 @@ public class PlayerHallwayRun : MonoBehaviour
     public float sanOpenDoorLimit = 10.0f;
     public float sanValueMaxium = 100.0f;
     public float sanValueAddSpeed = 20.0f;
-    public float sanValueReduceSpeed = 25.0f;
+    public float sanValueReduceSpeed = 10.0f;
     public List<HallwayEnemy> enemies = new List<HallwayEnemy>();
     public AudioSource footstepAudio;
+    public float waitForSanDown = 5.0f;
+    public bool isWaiting = false;
 
     void Start()
     {
@@ -35,8 +37,21 @@ public class PlayerHallwayRun : MonoBehaviour
         }
         else
         {
-            reduceSanValue();
-            MuteFootstepAudio();
+            if (isWaiting)
+            {
+                return;
+            }
+
+            if (sanValue/sanValueMaxium == 1.0f && isWaiting == false)
+            {
+                isWaiting = true;
+                StartCoroutine(CountDownBeforeRedueceSan(waitForSanDown));
+            }
+            else
+            {
+                reduceSanValue();
+                MuteFootstepAudio();
+            }
         }
     }
 
@@ -61,4 +76,11 @@ public class PlayerHallwayRun : MonoBehaviour
     {
         footstepAudio.volume = 0;
     }
-}
+    
+    IEnumerator CountDownBeforeRedueceSan(float waitSeconds) //Next phase
+    {
+        yield return new WaitForSeconds(waitSeconds);
+        reduceSanValue();
+        isWaiting = false;
+    }
+} 
