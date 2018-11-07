c2b5af7cee33b225e8cd3e9145f728400d4f30a1
Connor <cc3237@nyu.edu>
2018-11-07 18:52:21 -0500

    Changed all openable doors into single objects, added key sounds to all keys, wrote loading scene script, adjusted wallpaper in long corridor.

---
+++ Assets/Script/ConnorScripts/LoadSceneScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		StartCoroutine(LoadYourAsyncScene());
+	}
+	
+	IEnumerator LoadYourAsyncScene()
+	{
+		// The Application loads the Scene in the background as the current Scene runs.
+		// This is particularly good for creating loading screens.
+		// You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+		// a sceneBuildIndex of 1 as shown in Build Settings.
+
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Scenes/Hospital_Hall_Scene2", LoadSceneMode.Additive);
+
+		// Wait until the asynchronous scene fully loads
+		while (!asyncLoad.isDone)
+		{
+			yield return null;
+		}
+	}
+}
