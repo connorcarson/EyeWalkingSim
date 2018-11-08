8439298bcaf7a976a720a338a4e587456886466f
Yukii <yukiizzzx@gmail.com>
2018-11-08 10:12:00 +0800

    Write a script for wall disappear

---
+++ Assets/Script/EyesClose/WallDisappear.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDisappear : MonoBehaviour
+{
+	private float waitTime=5.0f;
+	public GameObject[] wallLayer;
+	public bool WallDisappearShift;
+	private int layerNum = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (WallDisappearShift)
+		{
+			WallDis();
+			WallDisappearShift = false;
+		}
+	}
+
+	void WallDis()
+	{
+		StartCoroutine(waitAndDisappear(waitTime));
+	}
+	
+	IEnumerator waitAndDisappear(float waitSeconds) //Next phase
+	{
+		yield return new WaitForSeconds(waitSeconds);
+		wallLayer[layerNum].SetActive(false);
+		layerNum++;
+		if (layerNum != wallLayer.Length)
+		{
+			StartCoroutine(waitAndDisappear(waitTime));
+		}
+	}
+}
